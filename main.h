3e96b5b9dda2f978c52bafbe04266c6c47d68e6f
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-17 08:52:14 -0700

    Create _printf

--- main.h
+++ main.h
@@ -6,6 +6,18 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * struct identifier - it holds the variables
+ *
+ * @fp: format id
+ * @ptr: function pointer
+ */
+typedef struct identifier
+{
+	char fp;
+	int (*ptr)(va_list);
+} identifier;
+
 int _printf(const char *format, ...);
 int char_format(va_list arg);
 
