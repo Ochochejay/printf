fa58a07ea77919204954373858c8622a7550ffbc
Chiamaka-Okek <chiamakarokeke@gmail.com>
2022-10-18 06:39:59 -0700

    Print integers

--- format_functions.c
+++ format_functions.c
@@ -35,3 +35,45 @@ int str_format(va_list arg)
 
 	return (len);
 }
+
+/**
+ * int_format - prints integers
+ *
+ * @arg: va_list of integers
+ * Return: number of bytes
+ */
+int int_format(va_list arg)
+{
+	int len = 0, i, num;
+	int c;
+	int buf[1000000];
+
+	num = va_arg(arg, int);
+	if (num < 0)
+	{
+		num *= -1;
+		write(1, "-", 1);
+		len++;
+	}
+
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+
+	while (num)
+	{
+		buf[len] = num % 10;
+		num = num / 10;
+		len++;
+	}
+	i = len - 1;
+
+	for (; i >= 0; i--)
+	{
+		c = buf[i] + 48;
+		write(1, &c, 1);
+	}
+	return (len);
+}
